68cb34678ad7a2b376df75119db4a77899ee92f8
Andrew Judd <awjudd@gmail.com>
2021-04-14 17:38:43 -0400

    feat(miner): miner details
    * Adding in new columns for you to provide with miners
    * Changing nullability of the "miner_id" column as it isn't used

---
+++ database/migrations/2021_04_14_074716_add_extra_dates_to_miners_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddExtraDatesToMinersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('miners', function(Blueprint $table) {
+            $table->unsignedBigInteger('miner_type_id')
+                    ->nullable()
+                    ->change();
+        });
+
+        Schema::table('miners', function (Blueprint $table) {
+            $table->date('purchase_date')
+                ->after('amount_paid')
+                ->nullable();
+            $table->date('estimated_activation_date')
+                ->after('purchase_date')
+                ->nullable();
+            $table->date('activation_date')
+                ->after('estimated_activation_date')
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('miners', function (Blueprint $table) {
+            $table->dropColumn([
+                'purchase_date',
+                'estimated_activation_date',
+                'activation_date',
+            ]);
+        });
+
+        Schema::table('miners', function(Blueprint $table) {
+            $table->unsignedBigInteger('miner_type_id')
+                ->nullable(false)
+                ->change();
+        });
+    }
+}
